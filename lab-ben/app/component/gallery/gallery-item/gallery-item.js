1ebb7ef056964a97da5b78e86943a5bcbf5265f4
Benjamin <ayzenbergb@gmail.com>
2017-06-08 11:43:09 -0700

    deletes all picures inside when a gallery is deleted

--- lab-ben/app/component/gallery/gallery-item/gallery-item.js
+++ lab-ben/app/component/gallery/gallery-item/gallery-item.js
@@ -5,13 +5,14 @@ require('./_gallery-item.scss');
 module.exports = {
   template: require('./gallery-item.html'),
   controllerAs: 'galleryItemCtrl',
-  controller: ['$log', 'galleryService', function($log, galleryService) {
+  controller: ['$log', 'galleryService', 'picService', function($log, galleryService , picService) {
     this.$onInit = () => {
       $log.debug('Gallery Item Controller');
 
       this.showEditGallery = false;
 
       this.deleteGallery = () => {
+        this.gallery.pics.forEach(pic => picService.deletePic(this.gallery, pic._id));
         galleryService.deleteGallery(this.gallery._id);
       };
     };
