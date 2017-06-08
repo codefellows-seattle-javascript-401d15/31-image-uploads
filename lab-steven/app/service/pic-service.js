b2422cd21cbb2693e1922433ed5cdfda2371b46c
Steven Johnson <stevenjohnson86@gmail.com>
2017-06-07 17:56:20 -0700

    review and add picUpload, thumbnail, tumbnailContainer components and pic service

--- lab-steven/app/service/pic-service.js
+++ lab-steven/app/service/pic-service.js
@@ -1,1 +1,46 @@
 'use strict';
+
+module.exports = ['$log', '$q', '$http', 'Upload', 'authService',
+  function($log, $q, $http, Upload, authService){
+    $log.debug('Pic Service');
+
+    let service = {};
+
+    service.uploadPic = function(gallery, pic){
+      $log.debug('#uploadPic');
+
+      return authService.getToken()
+      .then(token => {
+        let url = `${__API_URL__}/api/gallery/${gallery._id}/pic`;
+        let headers = {
+          Accept: 'application/json',
+          Authorization: `Bearer ${token}`,
+        };
+
+        return Upload.upload({
+          url,
+          headers,
+          method: 'POST',
+          data: {
+            name: pic.name,
+            desc: pic.desc,
+            file: pic.file,
+          },
+        });
+      })
+      .then(res => {
+        gallery.pics.push(res.data);
+        return res.data;
+      })
+      .catch(err => {
+        $log.error(err.message);
+        $q.reject(err);
+      });
+    };
+
+    // service.deletePic = function(gallery, pic){
+    //
+    // };
+
+    return service;
+  }];
