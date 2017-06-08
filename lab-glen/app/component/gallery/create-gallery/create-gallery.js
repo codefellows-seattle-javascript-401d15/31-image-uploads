a1583eb034e6320a257b3f833db3a0ebf2fae367
galang pham <phamgalang@gmail.com>
2017-06-08 13:21:54 -0700

    turn in

--- lab-glen/app/component/gallery/create-gallery/create-gallery.js
+++ lab-glen/app/component/gallery/create-gallery/create-gallery.js
@@ -20,7 +20,6 @@ module.exports = {
         })
         .catch(err => $log.error(err));
       };
-
     };
   }],
 };
--- lab-glen/app/component/gallery/gallery-item/gallery-item.js
+++ lab-glen/app/component/gallery/gallery-item/gallery-item.js
@@ -4,6 +4,7 @@ module.exports = {
   template: require('./gallery-item.html'),
   controllerAs: 'galleryItemCtrl',
   controller: ['$log', 'galleryService', function($log, galleryService) {
+
     $log.debug('Gallery Item Controller');
 
     this.showEditGallery = false;
