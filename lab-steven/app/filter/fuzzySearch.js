b50d58344f944721debbe6c84375a8710d387651
Steven Johnson <stevenjohnson86@gmail.com>
2017-06-13 12:10:50 -0700

    added fuzzySearch filter

---
+++ lab-steven/app/filter/fuzzySearch.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = function() {
+  return function(galleries, searchStr){
+
+    if(!searchStr) return galleries;
+
+    let createRegExp = new RegExp(`.*${searchStr.toLowerCase().split().join('.*')}.*`);
+    return galleries.filter(gal => createRegExp.test(gal.name.toLowerCase()));
+
+  };
+};
